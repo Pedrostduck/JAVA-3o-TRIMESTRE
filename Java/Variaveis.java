d2efe3619b18a9f858637e909c22aa8276f426f2
Pedrostduck <160145564+Pedrostduck@users.noreply.github.com>
2025-08-07 09:51:50 -0300

    Add files via upload

---
+++ Java/Variaveis.java
@@ -0,0 +1,19 @@
+public class Variaveis {
+    public static void main(String[] args) {
+        //tipo, nome, sinal = conteudo;
+
+        String MeuNome = "Pedro";
+        MeuNome += " Semensato";
+        int idade = 18;
+        double altura = 1.79;
+        char sexo = 'M';
+        boolean casado = false;
+
+
+        System.out.print("Meu Nome é "+MeuNome );
+        System.out.print("minha idade é "+idade);
+        System.out.print("anos, e minha altura é "+altura);
+        System.out.print(" sou casado? "+casado);
+        System.out.print(" sou do sexo "+ sexo);
+    }
+}
