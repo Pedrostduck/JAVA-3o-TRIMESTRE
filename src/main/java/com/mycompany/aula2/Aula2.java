fc56c025eb23f6d25a1b99343988de1ddf7db5de
Pedrostduck <160145564+Pedrostduck@users.noreply.github.com>
2025-08-29 09:05:03 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/aula2/Aula2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aula2;
+
+/**
+ *
+ * @author aluno
+ */
+public class Aula2 {
+
+    public static void main(String[] args) {
+        
+        pessoa eu = new pessoa();
+       // eu.nome = "Jorge";
+        System.out.println("Meu nome é " + eu.nome + ", sou do Sexo " + eu.sexo + ", estou " + eu.estado + ", meu hobbie é " + eu.hobbie + " e faz sentido pois ja estou com " + eu.idade + " anos.");
+
+    }
+}
---
+++ src/main/java/com/mycompany/aula2/pessoa.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.aula2;
+
+/**
+ *
+ * @author aluno
+ */
+public class pessoa {
+    
+    String nome = "Wesley";
+    char sexo = 'M';
+    int idade = 29;
+    String estado = "Casado";
+    String hobbie = "Viajar";
+    
+}
